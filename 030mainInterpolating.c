812411b8fa62b52891d9c751032caa8d5034d8c7
Keller Flint <kflint0068@gmail.com>
2017-01-11 13:15:28 -0600

    Replaced doubles with arrays where possible.

--- 030mainInterpolating.c
+++ 030mainInterpolating.c
@@ -9,7 +9,13 @@ int main() {
     if (pixInitialize(512, 512, "Pixel Graphics") != 0)
 		return 1;
 	else {
-        // TODO
+        double a[2] = {0, 0};
+        double b[2] = {6, 0};
+        double c[2] = {3, 6};
+
+        double rgb[3] = {1, 0, 0};
+
+        triRender(a, b, c, rgb);
 
         pixRun();
 		return 0;
--- 030triangle.c
+++ 030triangle.c
@@ -56,103 +56,108 @@ double findMiddle(double x, double y, double z) {
 }
 
 /* Returns the slope of a line given two points */
-double findSlope(double a0, double a1, double b0, double b1) {
-    return (b1 - a1)/(b0 - a0);
+double findSlope(double a[], double b[]) {
+    return (b[1] - a[1])/(b[0] - a[0]);
 }
 
-void render(double a0, double a1, double b0, double b1, double c0, double c1, double r, double g, double b) {
-    double x1, x2;
-    x1 = a0;
-    x2 = b0;
-
-    printf("First time x1: %f, x2: %f\n", x1, x2);
-
-    if (c1 == findLowOfTwo(a1, c1)) {
-        for (int y = findHigh(a1, b1, c1); y >= findLow(a1, b1, c1); y--) {
-            printf("y: %d\n", y);
-            x1 = x1 - (1/findSlope(a0, a1, c0, c1));
-            x2 = x2 - (1/findSlope(b0, b1, c0, c1));
-            printf("\tx1: %f, x2: %f\n", x1, x2);
-            for (int x = x1; x < x2; x++) {
-                pixSetRGB(x, y, r, g, b);
-            }
-        }
-    } else {
-        for (int y = findLow(a1, b1, c1); y <= findHigh(a1, b1, c1); y++) {
-            printf("y: %d\n", y);
-            x1 = x1 + (1/findSlope(a0, a1, c0, c1));
-            x2 = x2 + (1/findSlope(b0, b1, c0, c1));
-            printf("\tx1: %f, x2: %f\n", x1, x2);
-            for (int x = x1; x < x2; x++) {
-                pixSetRGB(x, y, r, g, b);
-            }
-        }
-    }
-}
-
-void triRender(double a0, double a1, double b0, double b1, double c0, double c1, double red, double green, double blue) {
+void render(double a0, double a1, double b0, double b1, double c0, double c1, double rgb[]) {
 
     double a[2] = {a0, a1};
     double b[2] = {b0, b1};
     double c[2] = {c0, c1};
 
-    if (findMiddle(a[1], b[1], c[1]) == -1) {
-        if (a[1] == findLow(a[1], b[1], c[1]) && c[1] == findLow(a[1], b[1], c[1])) {
-            b0 = c[0];
-            b1 = c[1];
-            c0 = b[0];
-            c1 = b[1];
-        } else if (b[1] == findLow(a[1], b[1], c[1]) && c[1] == findLow(a[1], b[1], c[1])) {
-            a0 = c[0];
-            a1 = c[1];
-            c0 = a[0];
-            c1 = a[1];
-        } else if (a[1] == findHigh(a[1], b[1], c[1]) && c[1] == findHigh(a[1], b[1], c[1])) {
-            b0 = c[0];
-            b1 = c[1];
-            c0 = b[0];
-            c1 = b[1];
-        } else if (b[1] == findHigh(a[1], b[1], c[1]) && c[1] == findHigh(a[1], b[1], c[1])) {
-            a0 = c[0];
-            a1 = c[1];
-            c0 = a[0];
-            c1 = a[1];
+    double x1, x2;
+    x1 = a[0];
+    x2 = b[0];
+
+    printf("First time x1: %f, x2: %f\n", x1, x2);
+
+    if (c[1] == findLowOfTwo(a[1], c[1])) {
+        for (int y = findHigh(a[1], b[1], c[1]); y >= findLow(a[1], b[1], c[1]); y--) {
+            printf("y: %d\n", y);
+            x1 = x1 - (1/findSlope(a, c));
+            x2 = x2 - (1/findSlope(b, c));
+            printf("\tx1: %f, x2: %f\n", x1, x2);
+            for (int x = x1; x < x2; x++) {
+                pixSetRGB(x, y, rgb[0], rgb[1], rgb[2]);
+            }
         }
     } else {
-        if (a[1] == findLow(a[1], b[1], c[1])) {
-            b0 = a[0];
-            b1 = a[1];
-        } else if (c[1] == findLow(a[1], b[1], c[1])) {
-            b0 = c[0];
-            b1 = c[1];
+        for (int y = findLow(a[1], b[1], c[1]); y <= findHigh(a[1], b[1], c[1]); y++) {
+            printf("y: %d\n", y);
+            x1 = x1 + (1/findSlope(a, c));
+            x2 = x2 + (1/findSlope(b, c));
+            printf("\tx1: %f, x2: %f\n", x1, x2);
+            for (int x = x1; x < x2; x++) {
+                pixSetRGB(x, y, rgb[0], rgb[1], rgb[2]);
+            }
+        }
+    }
+}
+
+void triRender(double a[2], double b[2], double c[2], double rgb[3]) {
+
+    double aa[2] = {a[0], a[1]};
+    double bb[2] = {b[0], b[1]};
+    double cc[2] = {c[0], c[1]};
+
+    if (findMiddle(aa[1], bb[1], cc[1]) == -1) {
+        if (aa[1] == findLow(aa[1], bb[1], cc[1]) && cc[1] == findLow(aa[1], bb[1], cc[1])) {
+            b[0] = cc[0];
+            b[1] = cc[1];
+            c[0] = bb[0];
+            c[1] = bb[1];
+        } else if (bb[1] == findLow(aa[1], bb[1], cc[1]) && cc[1] == findLow(aa[1], bb[1], cc[1])) {
+            a[0] = cc[0];
+            a[1] = cc[1];
+            c[0] = aa[0];
+            c[1] = aa[1];
+        } else if (aa[1] == findHigh(aa[1], bb[1], cc[1]) && cc[1] == findHigh(aa[1], bb[1], cc[1])) {
+            b[0] = cc[0];
+            b[1] = cc[1];
+            c[0] = bb[0];
+            c[1] = bb[1];
+        } else if (bb[1] == findHigh(aa[1], bb[1], cc[1]) && cc[1] == findHigh(aa[1], bb[1], cc[1])) {
+            a[0] = cc[0];
+            a[1] = cc[1];
+            c[0] = aa[0];
+            c[1] = aa[1];
+        }
+    } else {
+        if (aa[1] == findLow(aa[1], bb[1], cc[1])) {
+            b[0] = aa[0];
+            b[1] = aa[1];
+        } else if (cc[1] == findLow(aa[1], bb[1], cc[1])) {
+            b[0] = cc[0];
+            b[1] = cc[1];
         }
 
-        if (b[1] == findMiddle(a[1], b[1], c[1])) {
-            a0 = b[0];
-            a1 = b[1];
-        } else if (c[1] == findMiddle(a[1], b[1], c[1])) {
-            a0 = c[0];
-            a1 = c[1];
+        if (bb[1] == findMiddle(aa[1], bb[1], cc[1])) {
+            a[0] = bb[0];
+            a[1] = bb[1];
+        } else if (cc[1] == findMiddle(aa[1], bb[1], cc[1])) {
+            a[0] = cc[0];
+            a[1] = cc[1];
         }
 
-        if (a[1] == findHigh(a[1], b[1], c[1])) {
-            c0 = a[0];
-            c1 = a[1];
-        } else if (b[1] == findHigh(a[1], b[1], c[1])) {
-            c0 = b[0];
-            c1 = b[1];
+        if (aa[1] == findHigh(aa[1], bb[1], cc[1])) {
+            c[0] = aa[0];
+            c[1] = aa[1];
+        } else if (bb[1] == findHigh(aa[1], bb[1], cc[1])) {
+            c[0] = bb[0];
+            c[1] = bb[1];
         }
     }
 
-    printf("(%f,%f):(%f,%f):(%f,%f)\n", a0, a1, b0, b1, c0, c1);
+    printf("(%f,%f):(%f,%f):(%f,%f)\n", a[0], a[1], b[0], b[1], c[0], c[1]);
 
-    if (findMiddle(a[1], b[1], c[1]) != -1) {
+    if (findMiddle(aa[1], bb[1], cc[1]) != -1) {
         printf("Ran for middle\n");
-        double a2 = b0 + ((a1 - b1) * (1/findSlope(b0, b1, c0, c1)));
-        render(findLowOfTwo(a0, a2), a1, findHighOfTwo(a0, a2), a1, b0, b1, red, green, blue);
-        render(findLowOfTwo(a0, a2), a1, findHighOfTwo(a0, a2), a1, c0, c1, red, green, blue);
+        double a2 = b[0] + ((a[1] - b[1]) * (1/findSlope(b, c)));
+        render(findLowOfTwo(a[0], a2), a[1], findHighOfTwo(a[0], a2), a[1], b[0], b[1], rgb);
+        render(findLowOfTwo(a[0], a2), a[1], findHighOfTwo(a[0], a2), a[1], c[0], c[1], rgb);
     } else {
         printf("Ran for no middle\n");
-        render(findLowOfTwo(a0, b0), a1, findHighOfTwo(a0, b0), b1, c0, c1, red, green, blue);
+        render(findLowOfTwo(a[0], b[0]), a[1], findHighOfTwo(a[0], b[0]), b[1], c[0], c[1], rgb);
     }
 }
